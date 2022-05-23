bea80603351c7b179f4379d632d1f0594966d936
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-05-23 15:13:18 +0200

    updated readme and updated sprites for female version

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ Note: For this exercise, the goal is to keep working on it, until the deadline i
 Extra challenge
 There are a couple of pokemon that don't play with the normal rules, add code so their cases are also handled elegantly.
 
-* - [ ] Ditto only has 1 move.
+* - [x] Ditto only has 1 move.
 * - [ ] Eevee has 6 evolutions.
 
 ---
--- js/main.js
+++ js/main.js
@@ -13,14 +13,36 @@ document.getElementById('search').addEventListener('click', async function () {
     let data = await response.json();
     console.log(data);
 
-
+// display data from api on page(pokedex)
+    //display name
     document.getElementById('name').innerHTML = data.name;
+    //display ID
     document.getElementById('id').innerHTML = data.id;
+    // front and back sprites normal
     document.getElementById('frontSprite').src = data.sprites.front_default;
     document.getElementById('backSprite').src = data.sprites.back_default;
+    //front and back sprites Shiny version
     document.getElementById('frontSpriteShiny').src = data.sprites.front_shiny;
     document.getElementById('backSpriteShiny').src = data.sprites.back_shiny;
-    document.getElementById('type').innerHTML = data.types.map(type => type.type.name).join(", ");
+    //front and back sprites for female
+    document.getElementById('frontSpriteFemale').src = data.sprites.front_female;
+    document.getElementById('backSpriteFemale').src = data.sprites.back_female;
+    // front and back sprites shiny version female
+    document.getElementById('frontSpriteShinyFemale').src = data.sprites.front_shiny_female;
+    document.getElementById('backSpriteShinyFemale').src = data.sprites.back_shiny_female;
+    //Ditto only has 1 move
+    if (search === 132 || search === "ditto"){
+        document.getElementById('type').innerHTML = data.types.map(type => type.type.name).join(", ");
+        let movesArray = data.moves.map(move => move.move.name);
+        console.log(movesArray)
+        document.getElementById("moves").innerHTML = movesArray;
+    }
 
+    document.getElementById('type').innerHTML = data.types.map(type => type.type.name).join(", ");
+    let movesArray = data.moves.map(move => move.move.name).slice(0,4);
+    console.log(movesArray)
+    document.getElementById("moves").innerHTML = movesArray
 })
+
+
 
