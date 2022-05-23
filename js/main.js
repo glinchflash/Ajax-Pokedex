485397229bf41f18a7918d0ac258198775ae564e
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-05-23 14:02:13 +0200

    cleaned up js code, getting data from api fixed

--- js/main.js
+++ js/main.js
@@ -1,32 +1,26 @@
-//variable list
-const promises = [];
-let displayPoke = pokemon => {}
-//getting api
-for (let i = 1; i <= 151; i++) {
-    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-    promises.push(fetch(url).then(res => res.json()));
-}
-Promise.all(promises).then(results => {
-    console.log(results);
-    // gettind data from api
-    const pokemon = results.map(data => ({
-        name: data.name,
-        id: data.id,
-        frontImage: data.sprites["front_default"],
-        frontImageShiny: data.sprites["front_shiny"],
-        backImage: data.sprites["back_default"],
-        backImageShiny: data.sprites["back_shiny"],
-        type: data.types.map(type => type.type.name).join(", "),
-        moves:data.moves,
-    }));
-    console.log(pokemon);
-    document.getElementById('name').innerHTML = pokemon[0].name;
-    document.getElementById('frontSprite').src = pokemon[0].frontImage;
-    document.getElementById('backSprite').src = pokemon[0].backImage;
-    document.getElementById('frontSpriteShiny').src = pokemon[0].frontImageShiny;
-    document.getElementById('backSpriteShiny').src = pokemon[0].backImageShiny;
-    document.getElementById('type').innerHTML = pokemon[0].type;
 
-});
+//enable button to be clicked by pressing Enter key
+document.getElementById('searchInput').addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        document.getElementById('search').click();
+    }
+})
+//get input from user from api
+document.getElementById('search').addEventListener('click', async function () {
+    let search = document.getElementById('searchInput').value;
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${search}`);
+    let data = await response.json();
+    console.log(data);
 
+
+    document.getElementById('name').innerHTML = data.name;
+    document.getElementById('id').innerHTML = data.id;
+    document.getElementById('frontSprite').src = data.sprites.front_default;
+    document.getElementById('backSprite').src = data.sprites.back_default;
+    document.getElementById('frontSpriteShiny').src = data.sprites.front_shiny;
+    document.getElementById('backSpriteShiny').src = data.sprites.back_shiny;
+    document.getElementById('type').innerHTML = data.types.map(type => type.type.name).join(", ");
+
+})
 
